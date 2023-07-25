f7c49fb3ac5a088180b747475f131b0cca99d98d
Fatih Küçük <fkucuk@gmail.com>
2023-07-25 22:23:46 +0300

    moved to methods

--- Test1/AppUser.cs
+++ Test1/AppUser.cs
@@ -14,4 +14,3 @@ public class AppUser : IAppUser
         Email = email;
     }
 }
-
--- Test1/AuditableHelper.cs
+++ Test1/AuditableHelper.cs
@@ -6,36 +6,37 @@ namespace Test1;
 //Can be registered as transient
 public class AuditableHelper : IAuditableHelper
 {
-    private IAppUser currentUser;
+
+    private readonly IServiceScope _serviceScope;
 
     public AuditableHelper(IServiceScopeFactory serviceScopeFactory)
     {
-        using (var serviceScope = serviceScopeFactory.CreateScope())
-        {
-             currentUser = serviceScope.ServiceProvider.GetRequiredService<IAppUser>();
-        }
+        _serviceScope = serviceScopeFactory.CreateScope();
 
     }
 
     public void SetCreatedProperties(IAuditable auditable)
     {
+        var appUser = _serviceScope.ServiceProvider.GetRequiredService<IAppUser>();
         auditable.CreatedDateUtc = DateTime.UtcNow;
-        auditable.CreatedUser = GetUserIdentifierForAudit(currentUser);
+        auditable.CreatedUser = GetUserIdentifierForAudit(appUser);
     }
 
 
     public void SetDeletedProperties(IAuditable auditable)
     {
+        var appUser = _serviceScope.ServiceProvider.GetRequiredService<IAppUser>();
         auditable.DeletedDateUtc = DateTime.UtcNow;
-        auditable.DeletedUser = GetUserIdentifierForAudit(currentUser);
+        auditable.DeletedUser = GetUserIdentifierForAudit(appUser);
         auditable.IsDeleted = true;
     }
 
 
     public void SetUpdatedProperties(IAuditable auditable)
     {
+        var appUser = _serviceScope.ServiceProvider.GetRequiredService<IAppUser>();
         auditable.UpdatedDateUtc = DateTime.UtcNow;
-        auditable.UpdatedUser = GetUserIdentifierForAudit(currentUser);
+        auditable.UpdatedUser = GetUserIdentifierForAudit(appUser);
     }
 
     private string GetUserIdentifierForAudit(IAppUser currentUser)
